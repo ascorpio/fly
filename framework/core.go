7868e36c92c6155e2bf1df62143c1194c8fecfff
ascorpio <ascorpio@qq.com>
2022-08-12 17:05:58 +0800

    方法 + 静态路由匹配 + 分组

--- framework/core.go
+++ framework/core.go
@@ -1,37 +1,77 @@
 package framework
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 // Core 框架核心
 type Core struct {
-	router map[string]ControllerHandler
+	router map[string]map[string]ControllerHandler
 }
 
 // NewCore 初始化框架核心结构
 func NewCore() *Core {
+	router := map[string]map[string]ControllerHandler{
+		"GET":    {},
+		"POST":   {},
+		"PUT":    {},
+		"DELETE": {},
+	}
 	return &Core{
-		router: map[string]ControllerHandler{},
+		router: router,
 	}
 }
 
 func (c *Core) Get(url string, handler ControllerHandler) {
-	c.router[url] = handler
+	c.router["GET"][strings.ToUpper(url)] = handler
+}
+
+func (c *Core) Post(url string, handler ControllerHandler) {
+	c.router["POST"][strings.ToUpper(url)] = handler
+}
+
+func (c *Core) Put(url string, handler ControllerHandler) {
+	c.router["PUT"][strings.ToUpper(url)] = handler
+}
+
+func (c *Core) Delete(url string, handler ControllerHandler) {
+	c.router["DELETE"][strings.ToUpper(url)] = handler
 }
 
 // 框架核心结构实现 Handle 接口
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	log.Println("core.serverHTTP")
+	// 自定义 Context
 	ctx := NewContext(request, response)
 
 	// 一个简单的路由选择器，这里直接写死为测试路由foo
-	router := c.router["foo"]
+	router := c.FindRouteByRequest(request)
 	if router == nil {
+		// 如果没有找到，这里打印日志
+		ctx.Json(404, "not found")
 		return
 	}
-	log.Println("core.router")
 
-	router(ctx)
+	// 调用路由函数，如果返回err 代表存在内部错误，返回500状态码
+	if err := router(ctx); err != nil {
+		ctx.Json(500, "inner error")
+		return
+	}
+}
+
+// FindRouteByRequest 匹配路由，如果没有匹配到，返回nil
+func (c *Core) FindRouteByRequest(request *http.Request) ControllerHandler {
+	upperUri := strings.ToUpper(request.URL.Path)
+	upperMethod := strings.ToUpper(request.Method)
+	if methodHandlers, ok := c.router[upperMethod]; ok {
+		if handler, ok := methodHandlers[upperUri]; ok {
+			return handler
+		}
+	}
+	return nil
+}
+
+// Group 初始化分组
+func (c *Core) Group(prefix string) IGroup {
+	return NewGroup(c, prefix)
 }
---
+++ framework/group.go
@@ -0,0 +1,43 @@
+package framework
+
+// IGroup 代表前缀分组
+type IGroup interface {
+	Get(string, ControllerHandler)
+	Post(string, ControllerHandler)
+	Put(string, ControllerHandler)
+	Delete(string, ControllerHandler)
+}
+
+// Group 实现 IGroup 接口
+type Group struct {
+	core   *Core
+	prefix string
+}
+
+// NewGroup 初始化
+func NewGroup(core *Core, prefix string) *Group {
+	return &Group{
+		core:   core,
+		prefix: prefix,
+	}
+}
+
+func (g *Group) Get(uri string, handler ControllerHandler) {
+	uri = g.prefix + uri
+	g.core.Get(uri, handler)
+}
+
+func (g *Group) Post(uri string, handler ControllerHandler) {
+	uri = g.prefix + uri
+	g.core.Post(uri, handler)
+}
+
+func (g *Group) Put(uri string, handler ControllerHandler) {
+	uri = g.prefix + uri
+	g.core.Put(uri, handler)
+}
+
+func (g *Group) Delete(uri string, handler ControllerHandler) {
+	uri = g.prefix + uri
+	g.core.Delete(uri, handler)
+}
