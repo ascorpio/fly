6d8c530b3301f83259a58e019ab3d51bfa27564e
wjh <ascorpio@qq.com>
2022-08-28 23:23:53 +0800

    增加静态文件访问

--- app/http/route.go
+++ app/http/route.go
@@ -3,11 +3,12 @@ package http
 import (
 	"github.com/ascorpio/fly/app/http/module/demo"
 	"github.com/ascorpio/fly/framework/gin"
+	"github.com/ascorpio/fly/framework/middleware/static"
 )
 
 func Routes(r *gin.Engine) {
 
-	r.Static("/dist/", "./dist/")
+	r.Use(static.Serve("/", static.LocalFile("./vue_web/dist", false)))
 
 	demo.Register(r)
 }
